4afd727eee8d0f2526b1fd99e7bb3a8577208e10
Igor <igopood33@gmail.com>
2025-04-12 14:42:16 +0200

    initial commit

---
+++ GuesifyAPI/src/main/java/com/example/guesifyapi/GuesifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.guesifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuesifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuesifyApiApplication.class, args);
+    }
+
+}
