92d8d0b304f70ff94b8e024ee9fd15933e9bf125
Oliwia <okomor21@gmail.com>
2025-04-25 22:05:14 +0200

    feat: LoadingScreen added

--- mobile/Guessify/App.tsx
+++ mobile/Guessify/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './src/navigation/MainNavigator.tsx';
+import LoadingScreen from './src/screens/LoadingScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'react-native';
 import { AuthProvider } from './src/context/AuthContext.tsx';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <SafeAreaProvider>
       <StatusBar barStyle="dark-content" />
       <AuthProvider>
-        <NavigationContainer>
-          <MainNavigator />
-        </NavigationContainer>
+        {isLoading ? (
+          <LoadingScreen />
+        ) : (
+          <NavigationContainer>
+            <MainNavigator />
+          </NavigationContainer>
+        )}
       </AuthProvider>
     </SafeAreaProvider>
   );
--- mobile/Guessify/src/navigation/MainNavigator.tsx
+++ mobile/Guessify/src/navigation/MainNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoadingScreen from '../screens/LoadingScreen';
 import HomeScreen from '../screens/HomeScreen';
 import JoinRoomScreen from '../screens/JoinRoomScreen';
 import LoggedInHomeScreen from '../screens/LoggedInHomeScreen';
@@ -9,6 +10,7 @@ const Stack = createNativeStackNavigator();
 const MainNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+      <Stack.Screen name="Loading" component={LoadingScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="JoinRoom" component={JoinRoomScreen} />
       <Stack.Screen name="LoggedInHome" component={LoggedInHomeScreen} />
