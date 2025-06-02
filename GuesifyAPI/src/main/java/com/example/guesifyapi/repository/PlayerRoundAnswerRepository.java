632d093ff1a806713fa1fd1a597505f13cdfae9b
Igor <igopood33@gmail.com>
2025-06-03 01:02:59 +0200

    feat: repositories

---
+++ GuesifyAPI/src/main/java/com/example/guesifyapi/repository/PlayerRoundAnswerRepository.java
@@ -0,0 +1,7 @@
+package com.example.guesifyapi.repository;
+
+import com.example.guesifyapi.entity.PlayerRoundAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRoundAnswerRepository extends JpaRepository<PlayerRoundAnswer, Long> {
+}
